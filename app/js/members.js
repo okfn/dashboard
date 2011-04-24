2052040e6505d05ef0a33e26bbbbcce26ac9c06c
Maurizio Napolitano <napo@fbk.eu>
2011-04-25 00:25:38 +0200

    Changed the map  with OpenStreetMap using OpenLayers Added icons

--- app/js/members.js
+++ app/js/members.js
@@ -1,51 +1,89 @@
-var OKFN = {};
+	var map = new OpenLayers.Map("map");
+	map.addLayer(new OpenLayers.Layer.OSM());
+	var url = "./members.json";
+	var members;
+	var iconokfn = new OpenLayers.StyleMap({
+			'pointRadius': 10,
+			'externalGraphic': "img/okfn16.png"
+		});
+	var vectors = new OpenLayers.Layer.Vector("Members",
+		{styleMap:iconokfn,strategies: [new OpenLayers.Strategy.Cluster()]});
+		OpenLayers.loadURL(url, {}, null, function(r) {
+		var p = new OpenLayers.Format.JSON();
+		members = p.read(r.responseText);
+		var pointList = [];
+		for (key in members) {
+			var member = members[key];
+				if (member.geolocation != undefined) {
+					point  = new OpenLayers.Feature.Vector(
+								new OpenLayers.Geometry.Point(
+										member.geolocation.lng,member.geolocation.lat).transform(
+										new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject()));
+					point.attributes = {
+								userid: key,
+								name: member.Name,
+								location: member.Location
+					};
+						pointList.push(point);
+				}
+			}
+		vectors.addFeatures(pointList);
+	});
+	
+	function onPopupClose(evt) {
+		selectControl.unselect(selectedFeature);
+	}
 
-OKFN.Dashboard = function($) {
-  var my = {};
+	function onFeatureSelect(feature){
+		selectedFeature = feature;
+		var desc = "<html><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'><body>";
+		if (feature.cluster.length > 1) {
+			desc += "<strong>" + feature.cluster.length + " members in this area</strong><br/>";
+		}
+		for (var i=0; i < feature.cluster.length; ++i){
+			var userlink = 'http://okfn.org/members/' + feature.cluster[i].attributes.userid; 
+			desc += '<div style="width:240px;">';
+			desc += '<img src="img/okfnlogo.png" align="left"/><a target="_blank" href="' + userlink + '">' + feature.cluster[i].attributes.name + '</a>';
+			desc += '<br/>' + feature.cluster[i].attributes.location;
+			desc += '</div>';
+		}
+		if (feature.cluster.length > 1) {
+			desc += "<br/><em>tip: increase the zoom level</em>";
+		}
+		desc += "</body></html>";
+		popup = new OpenLayers.Popup.FramedCloud("chicken", 
+			feature.geometry.getBounds().getCenterLonLat(),
+			new OpenLayers.Size(1000,500),
+			desc,
+			null,
+			true,
+			onPopupClose); 
+	
+	feature.popup = popup;
+	map.addPopup(popup);
+	}
+	
+	function onFeatureUnselect(feature) {
+		map.removePopup(feature.popup);
+		feature.popup.destroy();
+		feature.popup = null;
+	}
+	
+	map.addLayers([vectors]);
+	selectControl = new OpenLayers.Control.SelectFeature(
+	[vectors],{clickout: true, toggle: false, 
+				multiple: false, hover: false, }
+	);
+	map.addControl(selectControl);
+	selectControl.activate();
+	vectors.events.on({
+			"featureselected": function(e) {
+				onFeatureSelect(e.feature);
+			},
+			"featureunselected": function(e) {
+				onFeatureUnselect(e.feature);
+			}
+	});
 
-  var geocoder;
-  var map;
-  my.membersMap = function() {
-    var latlng = new google.maps.LatLng(30, -20);
-    var myOptions = {
-      zoom: 3,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    $.getJSON('members.json', function(members) {
-      for (key in members) {
-        var member = members[key];
-        member.userid = key;
-        if (member.geolocation != undefined) { 
-          displayAddress(member);
-        }
-      }
-    });
-  };
- 
-  function displayAddress(member) {
-    var marker = new google.maps.Marker({
-      map: map, 
-      position: new google.maps.LatLng(member.geolocation.lat,
-        member.geolocation.lng),
-      title: member.Name
-    });
-    var userlink = 'http://okfn.org/members/' + member.userid; 
-      var html = '<div style="width:240px;"><a target="_blank" href="' + userlink + '">' +
-      member.Name + '</a>';
-      html += '<br />Location: ' + member.Location;
-      html += '</div>';
-    var infowindow = new google.maps.InfoWindow({
-      content: html,
-      size: new google.maps.Size(50,50)
-    });
-
-    google.maps.event.addListener(marker, "click", function() {
-      infowindow.open(map,marker);
-    });
-  }
-
-  return my;
-
-}(jQuery);
+	cp = new OpenLayers.Geometry.Point(0,0).transform(new OpenLayers.Projection("EPSG:4326")); 
+	map.setCenter(cp,2);
