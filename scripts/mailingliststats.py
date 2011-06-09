6a29878cab36c2067054a1bc7ef7eff372a0739f
Tim McNamara <tim.mcnamara@okfn.org>
2011-06-09 22:55:17 +1200

    Added scripts/mailingliststats.py
    Added a script that looks through all of OKFN's mailing lists and
    extracts datetimes for each of the posts. This data is currently
    just sent to stdout, but can easily be written to a file to be
    displayed as a graph.

---
+++ scripts/mailingliststats.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+"""
+This script tries to extract data from each of the OKFN mailing lists.
+"""
+
+import gzip
+import re
+import urllib2
+from urllib2 import urlopen
+from datetime import datetime
+from StringIO import StringIO
+DOMAIN = 'lists.okfn.org'
+ROOT_URL = 'http://{domain}/mailman/listinfo'.format(domain=DOMAIN)
+
+def iter_list_urls(listinfo_page=ROOT_URL):
+    """
+    >>> for l in iter_list_urls(): #doctest: +ELLIPSIS
+    ...     print l
+    annotator-dev
+    ...
+    wsfii-discuss
+    """
+    page = urlopen(listinfo_page).read()
+    for mailinglist in re.finditer('href="{p}/(.*)"'.format(p=listinfo_page), page):
+        yield mailinglist.groups()[0]
+
+def posts(list_name, domain=DOMAIN):
+    url = 'http://{domain}/pipermail/{list_name}/'.format(domain=domain, list_name=list_name)
+    print 'Fetching ...{url}'.format(url=url)
+    try:
+        page = urllib2.urlopen(url).read()
+    except urllib2.HTTPError as e:
+        if e.code >= 500:
+            raise
+        print e #probably a private list
+        page = ''
+    archives = [urllib2.urlopen(url+a) for a in re.findall('"(.*.txt.gz)', page)]
+    for archive in archives:
+        with gzip.GzipFile(fileobj=StringIO(archive.read())) as g:
+            # 'Thu May 12 21:32:47 2011'  
+            post_times = re.findall(' \w{3} [0-9]{2} [0-9:]{8} [0-9]{4}', g.read())
+            for t in post_times:
+                yield datetime.strptime(t, " %b %d %H:%M:%S %Y").isoformat()
+        
+
+def list_name_to_archive_index(name, domain=DOMAIN):
+    return 'http://{domain}/pipermail/{list_name}/'.format(domain=domain, list_name=name)
+
+def main():
+    for list_name in iter_list_urls():
+        for post in posts(list_name):
+            print post, list_name, 1
+
+if __name__ == "__main__":
+    main()
