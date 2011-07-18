db3e56346c3ed7fe5535549be0a73ab404355b97
Friedrich Lindenberg <friedrich@pudo.org>
2011-07-18 22:38:04 +0200

    fix typo

---
+++ gather/github.py
@@ -0,0 +1,23 @@
+import logging
+import urllib
+import json 
+
+URL = 'https://api.github.com/%ss/%s/repos'
+FEED = 'https://github.com/%s/%s/commits.atom'
+
+import feed
+
+log = logging.getLogger(__name__)
+
+def gather(database, name=None, account=None):
+    url = URL % (account, name)
+    log.info('%s: %s' % (account, name))
+    urlfh = urllib.urlopen(url)
+    data = json.load(urlfh)
+    urlfh.close()
+    for repo in data:
+        furl = FEED % (name, repo['name'])
+        feed.gather(database, url=furl)
+
+
+
