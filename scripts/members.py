8f128fa803019de8b35b95730a5506c13865e1db
rgrp <rufus.pollock@okfn.org>
2011-08-27 02:19:09 +0100

    [members][s]: (fixes #1) switch to spatial field for geo info and save geo info to webstore by serializing to json string.

--- scripts/members.py
+++ scripts/members.py
@@ -14,9 +14,12 @@ from collections import defaultdict
 from common import database
 from datautil.clitools import _main
 
+CSV_SOURCE = 'data/members.csv'
+MEMBERS_RAW = 'cache/members.raw.json'
+MEMBERS_GEO = 'cache/members.geo.json'
 
-def convert_from_csv_to_json(csv_location='data/members.csv',
-                             output_file='cache/members.raw.json'):
+def convert_from_csv_to_json(csv_location=CSV_SOURCE,
+        output_file=MEMBERS_RAW):
     fo = open(csv_location, 'rU')
     reader = csv.reader(fo)
     out = defaultdict(dict)
@@ -38,7 +41,7 @@ def convert_from_csv_to_json(csv_location='data/members.csv',
         out[username]['username'] = username
     for username in out:
         out[username] = normalize(out[username])
-    outfo = open(hack_file_location(output_file), 'w')
+    outfo = open(output_file, 'w')
     json.dump(out, outfo, indent=2, sort_keys=True)
 
 
@@ -56,7 +59,7 @@ def normalize(datadict_):
 
 def geocode_data():
     '''Geocode the string locations using geonames.'''
-    data = json.load(open(hack_file_location(FN)))
+    data = json.load(open(MEMBERS_RAW))
     baseurl = 'http://api.geonames.org/searchJSON?maxRows=1&username=demo&q='
     for key, value in data.items():
         if 'Location' in value:
@@ -68,19 +71,19 @@ def geocode_data():
             res = fo.read()
             res = json.loads(res)
             if res['geonames']:
-                data[key]['geolocation'] = res['geonames'][0]
+                data[key]['spatial'] = res['geonames'][0]
                 print 'Matched ok: %s to %s' % (loc,
                         res['geonames'][0]['name'].encode('utf8', 'ignore'))
             else:
                 print 'Failed to match: %s' % loc
             time.sleep(0.5) 
-    fileobj = open('cache/members.geo.json', 'w')
+    fileobj = open(MEMBERS_GEO, 'w')
     json.dump(data, fileobj, indent=2, sort_keys=True)
 
 
 def upload_to_webstore():
     table = database['person']
-    fileobj = open('cache/members.geo.json')
+    fileobj = open(MEMBERS_GEO)
     out = json.load(fileobj)
     count = 0
     for username, info in out.items():
@@ -88,8 +91,10 @@ def upload_to_webstore():
         data = dict(info)
         data['username'] = username
         data = normalize(data)
-        if 'geolocation' in data:
-            del data['geolocation']
+        if 'spatial' in data:
+            # have to dump to a string due to
+            # https://github.com/okfn/webstore/issues/25
+            data['spatial'] = json.dumps(data['spatial'])
         table.writerow(data, unique_columns=['username'])
         print 'Processed: %s (of %s)' % (count, len(out))
 
