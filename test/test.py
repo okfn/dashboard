03910e2c6ade393639f4cb27a3d26596f420c92c
Dinu Gherman <dinu@Flatty.local>
2011-08-27 13:40:26 +0200

    Tried starting a feed test, still incomplete, though.

--- test/test.py
+++ test/test.py
@@ -3,14 +3,21 @@
 
 import os
 import sys
+import ConfigParser
 import unittest
-
 
 # Add 'scripts' folder to module search path allowing to execute 
 # this test module from inside its 'test' folder.
 path = os.path.join(os.path.dirname(__file__), '..', 'scripts')
 sys.path.insert(0, path)
 del path
+
+from datautil.clitools import _main
+from webstore.client import Database
+
+import feed
+from common import database
+from normalize import normalize
 
 
 class DashboardTestCase(unittest.TestCase):
@@ -21,8 +28,15 @@ class DashboardTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test(self):
-        pass
+    def test_feed(self):
+        url = "http://blog.okfn.org/feed/"
+        feed.gather(database, url=url)
+        
+        config = ConfigParser.SafeConfigParser()
+        config.read(["../dashboard.cfg"])
+        normalize(database, config)
+
+        # Todo: now do the real testing...
 
 
 if __name__ == '__main__':
