6322aa309271503e83e3b6224b649eeb21c604aa
Éric Lemoine <eric.lemoine@gmail.com>
2012-01-25 21:47:46 +0100

    [app] move the member model in its own script

--- app/js/app.js
+++ app/js/app.js
@@ -1,30 +1,6 @@
 var Dashboard = Dashboard || {};
 Dashboard.Util = Dashboard.Util || {};
 
-// Class: Backbone model for a member
-Dashboard.Member = Backbone.Model.extend({
-  defaults: {
-    'name': 'John Doe',
-    'location': 'Nowhere', 
-    'geolocation': {
-      adminCode1: "07",
-      adminName1: "North Rhine-Westphalia",
-      countryCode: "DE",
-      countryName: "Germany",
-      fcl: "P",
-      fclName: "city, village,...",
-      fcode: "PPLA2",
-      fcodeName: "seat of a second-order administrative division",
-      geonameId: 2886242,
-      lat: 50.9333333,
-      lng: 6.95,
-      name: "Cologne",
-      population: 963395,
-      toponymName: "Köln"
-    }
-  }
-});
-
 // Class: Tabular view for members
 Dashboard.MemberTableView = Backbone.View.extend({
   initialize: function() {
