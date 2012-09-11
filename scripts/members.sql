1e29f46cfd545cba5b365ffe87f702381aa7bfe6
Tom Rees <zephod@gmail.com>
2012-09-11 14:31:31 +0100

    Cleaned out this branch. gh-pages will now be the deployment branch of an auto-generated web app kept in master.

--- scripts/members.sql
+++
@@ -1,6 +0,0 @@
-/* Mysql user running this script must be given the global 'FILE' grant. */
-
-SELECT okfn_org_users.user_login, okfn_org_bp_xprofile_fields.name, okfn_org_bp_xprofile_data.value
-FROM okfn_org_users join okfn_org_bp_xprofile_data on okfn_org_users.id=okfn_org_bp_xprofile_data.user_id join okfn_org_bp_xprofile_fields on field_id=okfn_org_bp_xprofile_fields.id
-;
-
