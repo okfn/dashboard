25dc79ecb091ccd80ed6a7167686c449be88e2d6
Rufus Pollock <rufus.pollock@okfn.org>
2013-03-15 13:40:28 +0000

    [build][xs]: .

--- javascripts/app.js
+++ javascripts/app.js
@@ -370,7 +370,7 @@ window.require.register("projects", function(exports, require, module) {
             title: 'School Of Data',
             twitter: 'schoolofdata',
             link: ['http://schoolofdata.org', 'http://handbook.schoolofdata.org', 'http://opendatahandbook.org', 'http://wiki.okfn.org/Projects/Open_Data_Handbook'],
-            mailman: ['School-of-data', 'school-of-data-announce'],
+            mailman: ['School-of-data', 'School-of-data-announce'],
             github: ['okfn/datawrangling', 'okfn/schoolofdata', 'okfn/opendatahandbook'],
             headline_github: 'okfn/schoolofdata',
             google_analytics: ['School Of Data']
